8909bb0c7d89b9ee228d08e044d75ff380c3895a
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-11-10 21:51:06 -0500

    Add thread safety with semaphore

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -2,24 +2,51 @@
 #include "get_frams.h"
 #include "ml.h"
 
-TaskHandle_t inferenceTaskHandle;
-TaskHandle_t getFramesTaskHandle;
+TaskHandle_t inferenceTaskHandle = nullptr;
+TaskHandle_t captureTaskHandle = nullptr;
 
-// Define the model inference thread
-void inference_thread(void* param){
+SemaphoreHandle_t frameMutex;
+volatile bool frame_ready = false;
+uint8_t framebuffer[FRAME_SIZE];
+
+
+
+// Thread 1: capture frames from serial or a camera
+void capture_thread(void* param) {
     while(true){
-        //get the next frame using the get_frame package
-        if(frame_ready){
-            frame_ready = false;
-            int8_t result = run_inference(frame_buffer, sizeof(frame_buffer));
-            Serial.print("Inference result: ");
-            Serial.println(result);
-
+        while(true) {
+            if(get_next_frame()) {
+                //atomically copy the frame data to the shared buffer
+                xSemaphoreTake(frameMutex, FRAME_SIZE);
+                frame_ready = true;
+                xSemaphoreGive(frameMutex);
+            }
         }
-        //avoid starving other tasks
+    }
+}
+
+// Thread 2: run inference on captured frames
+void inference_thread(void* param) {
+    while(true) {
+        //atomically consume the frame buffer 
+        bool ready = false;
+        xSemaphoreTake(frameMutex, portMAX_DELAY);
+        ready = frame_ready;
+        if (frame_ready) frame_ready = false;
+        xSemaphoreGive(frameMutex);
+
+        //write inference result to serial
+        if (ready) {
+            int8_t result = run_inference(framebuffer, sizeof(framebuffer));
+            Serial.print("Inference result; ");
+            Serial.println(result);
+        }
+
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
+
+
 
 void setup() {
 
@@ -32,9 +59,12 @@ void setup() {
     //Load the embedded tflite flatbuffer encoded model
     init_model();
 
+    //create sychronization semaphore
+    frameMutex = xSemaphoreCreateMutex();
+
     //Start the two demo threads (model inference and frame capture)
-    xTaskCreatePinnedToCore(serial_thread_task, "SerialThread", 4096, NULL, 1, &serialTaskHandle, 0);
-    xTaskCreatePinnedToCore(inference_thread_task, "InferenceThread", 8192, NULL, 1, &inferenceTaskHandle, 1);
+    xTaskCreatePinnedToCore(capture_thread, "CaptureThread", 4096, NULL, 1, &captureTaskHandle, 0);
+    xTaskCreatePinnedToCore(inference_thread, "InferenceThread", 8192, NULL, 1, &inferenceTaskHandle, 1);
 
 }
 
